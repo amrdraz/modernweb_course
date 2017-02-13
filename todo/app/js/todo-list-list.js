719a3727176591693f7633aa24ae734206d16acd
Amr Draz <amr.m.draz@gmail.com>
2017-02-13 14:20:05 +0200

    removed a hitn to the solution :P

--- todo/app/js/todo-list-list.js
+++ todo/app/js/todo-list-list.js
@@ -7,7 +7,7 @@
         <button type="button" class="create-list secondary-bg-color ">Create New List</button>
         <ul class="todo-list-list">
             ${todo_list_list.map((list, i)=>{
-              return `<li onclick="TodoListList.selectList(${i})" class="todo-list-list__item primary-bg-color">
+              return `<li class="todo-list-list__item primary-bg-color">
                 <div class="todo-list-list__todo-list row">
                   <span class="todo-list-list__title">${list.title}</span>
                   <button class="todo-list-list__delete-btn">x</button>
