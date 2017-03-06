12228d25f8fa44cd6eaafc2ee2907b2a223a8035
Amr Draz <amr.m.draz@gmail.com>
2017-03-06 12:20:49 +0200

    added action test

---
+++ 5.0.actionreducer/src/actions.test.js
@@ -0,0 +1,14 @@
+const actions = require('actions');
+
+
+test('creatList should return a create List action',  () => {
+  expect(actions.creatList("New List")).toEqual({
+    type: 'CREATE_LIST'
+    list: {
+      title: 'New List',
+      items: []
+    }
+  });
+});
+
+// Do the REST
