cd45e69a70d88327a3a180e22e7eda2bf5657f17
Amr Draz <amr.m.draz@gmail.com>
2017-04-03 16:17:05 +0200

    added cros origin support

--- 7.1.server/app.js
+++ 7.1.server/app.js
@@ -1,12 +1,16 @@
 // https://expressjs.com/en/starter/hello-world.html
 const express = require('express');
+// https://github.com/expressjs/body-parser
 const bodyParser = require('body-parser');
+// Google npm cors :P
+const cors = require('cors');
 
 const app = express()
 
 // https://github.com/expressjs/body-parser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
